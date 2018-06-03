4dc1a36ed8dd0545407ac6770caec25d7bfc1d57
Anwar Ul-Haq <aulhaq6@gmail.com>
2018-06-03 20:20:48 +0100

    Order item file update

--- AGSports/AGSports/Repositories/OrderItemRepository.cs
+++ AGSports/AGSports/Repositories/OrderItemRepository.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AGSports.Contracts;
+using AGSports.Models;
 
 namespace AGSports.Repositories
 {
     public class OrderItemRepository  : IOrderItemRepository
     {
+        private readonly AGSportsContext _context;
+
+        public OrderItemRepository(AGSportsContext context)
+        {
+            _context = context;
+        }
     }
 }
